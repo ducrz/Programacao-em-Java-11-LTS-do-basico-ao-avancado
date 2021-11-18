823efe30761d2468fa6c1c6911470a4419fac1bc
ducrz <ducatolico@gmail.com>
2021-11-18 11:05:22 -0300

    Fim Seção 05: Estruturas de Repetição em Java.

---
+++ GeekJava/src/geek/Programa10.java
@@ -0,0 +1,18 @@
+package geek;
+
+public class Programa10 {
+	
+	//Foreach
+
+	public static void main(String[] args) {
+
+		String nome = "Eduardo";
+		//Para cada caractere da string, imprima o caractere
+		for (char letra : nome.toCharArray() ) {
+			System.out.println(letra);
+			//System.out.print(letra);
+			
+		}
+	}
+
+}
