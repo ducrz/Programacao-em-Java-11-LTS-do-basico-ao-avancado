e0e2a56efb5744130a41cb54f7f6d75c137089ba
ducrz <ducatolico@gmail.com>
2021-11-23 12:06:05 -0300

    Fim Seção 16: Tratamento de Erros.

---
+++ GeekJava/src/br/com/geekuniversity/secao16/Programa36.java
@@ -0,0 +1,29 @@
+package br.com.geekuniversity.secao16;
+
+import java.util.Iterator;
+
+//Tratando exceções com Try/Catch
+public class Programa36 {
+
+	public static void main(String[] args) {
+		int numeros[] = new int[5];
+		
+		for (int i=0; i < numeros.length; i++) {
+			
+				numeros[i] = i +3 * 2;
+			
+		}
+		
+		for(int i =0; i < numeros.length; i++) {
+			try { //Tente fazer isto
+				System.out.println(numeros[i]);
+			}catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Você está tentando acessar uma posição do Array"
+						+ "que não existe!");
+				
+			}
+		}
+
+	}
+
+}
